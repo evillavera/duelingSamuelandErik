d8caaa0f8970045f5653c1d97c12980f0fe20779
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-09-14 10:59:20 -0400

    Character A

--- Duel Project Samuel and Erik/src/duel/CharacterA.java
+++ Duel Project Samuel and Erik/src/duel/CharacterA.java
@@ -2,16 +2,21 @@ package duel;
 
 public class CharacterA implements Dueler {
 	
+	private String name; 
+	
+	public CharacterA(String name) {
+		this.name=name;
+	}
+	
+	
 	public String getName() {
 		return "Erik Villavera ";
 	}
 
-	public void setStartingHP(int hp) {
-		
-	}
+	public void setStartingHP(int hp) {}
 	
 	public int getHP() {
-		return 0;
+		return 100;
 	}
 
 	public void taunt() {
