6f76472aa3defbc1b7bd78e588c48e34d33c1cee
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-09-14 11:40:48 -0400

    sdfasdfasdf

---
+++ Duel Project Samuel and Erik/src/duel/CharacterB.java
@@ -0,0 +1,22 @@
+package duel;
+
+public class CharacterB implements Dueler {
+	
+	public CharacterB() {
+	}
+	
+	
+	public String getName() {
+		return "Samuel Khong ";
+	}
+
+	public void setStartingHP(int hp) {}
+	
+	public int getHP() {
+		return 100;
+	}
+
+	public void taunt() {
+		
+	}
+	}
