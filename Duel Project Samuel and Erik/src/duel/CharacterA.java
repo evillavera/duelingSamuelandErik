9b29fe791937052035abe3888d6b9a32bff8f689
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-09-14 11:43:38 -0400

    adfasdf

--- Duel Project Samuel and Erik/src/duel/CharacterA.java
+++ Duel Project Samuel and Erik/src/duel/CharacterA.java
@@ -2,19 +2,23 @@ package duel;
 
 public class CharacterA implements Dueler {
 	
+	private int hp;
+	
 	public CharacterA() {}
 	
 	public String getName() {
 		return "Erik Villavera ";
 	}
 
-	public void setStartingHP(int hp) {}
+	public void setStartingHP(int hp) {
+		this.hp = hp;
+	}
 	
 	public int getHP() {
-		return 100;
+		return hp;
 	}
 
 	public void taunt() {
-		
+		System.out.println("You smell like poop! ");
 	}
 	}
